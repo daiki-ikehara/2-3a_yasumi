71546b00990e0ca26983bedacc8fe18095c8154b
KBC12005049\kbc12005049 <nanako-tanahara-20f@stu.lbc.ac.jp>
2022-01-24 09:33:24 +0900

    先生に当たったらゲームオーバーになる処理追加

---
+++ 2-3a_yasumi/Assets/Script/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] GameObject overPanel;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))//エネミーに当たった時に
+        {
+            overPanel.SetActive(true);
+            Debug.Log("当たった");
+            Time.timeScale = 0f;
+        }
+    }
+}
