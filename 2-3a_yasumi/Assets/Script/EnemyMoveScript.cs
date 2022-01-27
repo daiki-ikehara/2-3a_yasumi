7f957360012d30006d06637f1fe9fe1cd08bbd47
yoza-yuya <yuuya-yoza-20f@stu.kbc.ac.jp>
2022-01-27 09:58:47 +0900

    enemyscriptの変更更新

--- 2-3a_yasumi/Assets/Script/EnemyMoveScript.cs
+++ 2-3a_yasumi/Assets/Script/EnemyMoveScript.cs
@@ -8,6 +8,12 @@ public class EnemyMoveScript : MonoBehaviour
     GameObject Enemy;
     public float Speed;
 
+    private Rigidbody2D rbody2D;
+
+    private float jumpForce = 350f;
+
+    private int jumpCount = 0;
+
     private bool countflg;//カウント中敵を動かなくする(変更点)
 
 
@@ -16,6 +22,7 @@ public class EnemyMoveScript : MonoBehaviour
     {
         Player = GameObject.Find("player");
         Enemy = GameObject.Find("Enemy");
+        rbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -26,6 +33,19 @@ public class EnemyMoveScript : MonoBehaviour
         if (countflg == false)//変更点
         {
             this.transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(Player.transform.position.x, Player.transform.position.y), Speed * Time.deltaTime);
+        }
+        if (Input.GetMouseButtonDown(0) && this.jumpCount < 1)
+        {
+            this.rbody2D.AddForce(transform.up * jumpForce);
+            jumpCount++;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            jumpCount = 0;
         }
     }
 }
