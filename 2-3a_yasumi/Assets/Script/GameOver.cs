718e6a098933a3e0a012d1f41b493170ce67debd
KBC12005049\kbc12005049 <nanako-tanahara-20f@stu.lbc.ac.jp>
2022-01-27 11:35:22 +0900

    リトライボタン作ったけどカウント周りがよくわからんので直せる人直してほしい

--- 2-3a_yasumi/Assets/Script/GameOver.cs
+++ 2-3a_yasumi/Assets/Script/GameOver.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
     [SerializeField] GameObject overPanel;
+    public Button FirstSelectButton;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))//エネミーに当たった時に
         {
             overPanel.SetActive(true);
             Debug.Log("当たった");
-            Time.timeScale = 0f;
+            
+            FirstSelectButton.Select();
         }
     }
 }
