dcc90c9442e465e3d81ffff17b333372f240efb8
KBC1200-5156\kbc12005156 <reon-nakamura-20f@stu.kbc.ac.jp>
2022-01-27 15:07:57 +0900

    ２回目以降のカウントダウン時に敵とプレイヤーが動いてしまうバグを修正。クリア時にプレイヤーの動きを止める処理。２回目以降のゲームオーバーでプレイヤーがまだ動いてしまう。

--- 2-3a_yasumi/Assets/Script/GameManager.cs
+++ 2-3a_yasumi/Assets/Script/GameManager.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-   
+
+    public static bool retryflg = false;
+
     public void Retry()
     {
-        
+        retryflg = true;
         Scene loadScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         Time.timeScale = 1f;
+
 
     }
 }
--- 2-3a_yasumi/Assets/Script/PlayerControl.cs
+++ 2-3a_yasumi/Assets/Script/PlayerControl.cs
@@ -16,7 +16,6 @@ public class PlayerControl : MonoBehaviour
     private float speed;//x座標のキャラのスピード
     private bool jumpflg = false;
     private float height;
-    private float jumpPos = 0.0f;
     private bool setti = true;
     public float inputSpeed;
     public float JumpPower;
@@ -41,6 +40,7 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
 
+
         countflg = CoundDown.countflg;
 
         if (countflg == false)
@@ -50,69 +50,51 @@ public class PlayerControl : MonoBehaviour
 
 
 
-        //ジャンプ押したときの処理
+            //ジャンプ押したときの処理
 
-        if (setti == true)
-        {
-            if (Input.GetButtonDown("Jump"))//ジャンプ押すと
+            if (setti == true)
             {
-                rb2d.AddForce(transform.up * JumpPower, ForceMode2D.Impulse);
-                audioSource.PlayOneShot(JumpSound);
+                if (Input.GetButtonDown("Jump"))//ジャンプ押すと
+                {
+                    rb2d.AddForce(transform.up * JumpPower, ForceMode2D.Impulse);
+                    audioSource.PlayOneShot(JumpSound);
                     //jumpPos = transform.position.y;//ジャンプする前の座標を渡す
                     //height += JumpPower;//座標にジャンプの値を渡してジャンプ
                     jumpflg = true;//ジャンプフラグをture
-                setti = false;
+                    setti = false;
+                }
+            }
+            else//ジャンプボタンが押されていないとき
+            {
+                height -= gravity;//重力
+                jumpflg = false;//じゃんぷふらごをおふ
+
+            }
+
+
+            //待機
+            if (x_val == 0)
+            {
+                speed = 0;
+                anim.SetBool("right_run", false);
+                anim.SetBool("left_run", false);
+
+            }
+            //右に移動
+            else if (x_val > 0)
+            {
+                speed = inputSpeed;
+                anim.SetBool("right_run", true);
+                transform.position = new Vector2(transform.position.x + inputSpeed, transform.position.y);
+            }
+            //左に移動
+            else if (x_val < 0)
+            {
+                speed = inputSpeed * -1;
+                anim.SetBool("left_run", true);
+                transform.position = new Vector2(transform.position.x - inputSpeed, transform.position.y);
             }
         }
-        else//ジャンプボタンが押されていないとき
-        {
-            height -= gravity;//重力
-            jumpflg = false;//じゃんぷふらごをおふ
-
-        }
-
-        //if (jumpflg && jumpPos + jumpheight > transform.position.y)//ジャンプボタンが押されてるかつジャンプの範囲制御
-        //{
-        //    height += JumpPower;//ジャンプさせる
-        //    jumpflg = true;//ジャンプフラグ
-        //}
-        //else//ジャンプボタンが押されていないとき
-        //{
-        //    jumpflg = false;//ジャンプフラグオフ
-        //}
-
-        //待機
-        if (x_val == 0)
-        {
-            speed = 0;
-            anim.SetBool("right_run", false);
-            anim.SetBool("left_run", false);
-
-        }
-        //右に移動
-        else if (x_val > 0)
-        {
-            speed = inputSpeed;
-            anim.SetBool("right_run", true);
-            transform.position = new Vector2(transform.position.x + inputSpeed, transform.position.y);
-        }
-        //左に移動
-        else if (x_val < 0)
-        {
-            speed = inputSpeed * -1;
-            anim.SetBool("left_run", true);
-            transform.position = new Vector2(transform.position.x - inputSpeed, transform.position.y);
-        }
-    }
 
     }
-    //public void SetHp(int hp)
-    //{
-    //    this.hp = hp;
-    //}
-
-    //public int GetHp()
-    //{
-    //    return this.hp;
-    //}
 }
